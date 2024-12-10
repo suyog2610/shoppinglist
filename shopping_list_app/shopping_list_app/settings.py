b50a2411f38ea910d84ebaa558c35498e9808bfa
suyog2610 <suyogdoiphode@gmail.com>
2024-12-10 21:28:52 +0530

    added requirements file

--- shopping_list_app/shopping_list_app/settings.py
+++ shopping_list_app/shopping_list_app/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bc!8m6k!9w#hwz*f$g%z(a0*c##3(3-$mm6!7q58fj7$_m9zy='
+SECRET_KEY = '-insecure-bc!8m6k!django9w#hwz*f$g%z(a0*c##3(3-$mm6!7q58fj7$_m9zy='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
